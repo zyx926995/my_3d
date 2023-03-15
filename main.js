ab4171e2ecb2d24d56de11f1e0559fac0a79e816
zyx <zhangzhida_cool@126.com>
2023-03-15 19:47:01 +0800

    Add 3D model and web files

---
+++ main.js
@@ -0,0 +1,35 @@
+// Set up the scene, camera, and renderer
+const scene = new THREE.Scene();
+const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
+
+const renderer = new THREE.WebGLRenderer();
+renderer.setSize(window.innerWidth, window.innerHeight);
+document.body.appendChild(renderer.domElement);
+
+// Add a light
+const light = new THREE.PointLight(0xffffff, 1, 0);
+light.position.set(50, 50, 50);
+scene.add(light);
+
+// Load and display the 3D model
+const loader = new THREE.OBJLoader();
+loader.load(
+  'C:/Users/Lenovo/Documents/WeChat Files/wxid_is0ytlo9obt422/FileStorage/File/2023-03/clothes.obj', // Replace this with the path to your obj model
+  function (obj) {
+    scene.add(obj.scene);
+  },
+  undefined,
+  function (error) {
+    console.error(error);
+  }
+);
+
+// Set the camera position
+camera.position.z = 5;
+
+// Animate the scene
+function animate() {
+  requestAnimationFrame(animate);
+  renderer.render(scene, camera);
+}
+animate();
